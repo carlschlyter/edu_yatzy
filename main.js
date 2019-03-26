82d584e0f7348d19b51aa98ce87559593e78aaee
Carl Schlyter <carl.olof.schlyter@hotmail.com>
2019-03-26 11:20:27 +0100

    General developing

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+
 document.addEventListener('DOMContentLoaded', function(e){
-    
+  let btnCalc = document.getElementById('calc');
+  btnCalc.addEventListener('click', function(event){
+    alert('Hejsan javascript!');
+  });  
 });
